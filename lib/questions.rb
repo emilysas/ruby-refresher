d20210dcd15dbb815c67439cee6f0f4d15f21b34
Emily Sas <emily.bronwen.sas@gmail.com>
2015-02-09 15:30:19 +0000

    refactoring

--- lib/questions.rb
+++ lib/questions.rb
@@ -189,9 +189,7 @@ end
 # 'The Lion the Witch and the Wardrobe'
 def titleize_a_string(string)
   excluded = ['the', 'and', 'is', 'a', 'to', 'be', 'was', 'in']
-  string.capitalize.split(" ").map do |word| 
-    excluded.include?(word) ? word : word.capitalize}.join(" ")
-  end
+  string.capitalize.split(" ").map{|word| excluded.include?(word) ? word : word.capitalize}.join(" ")
 end
 
 # return true if a string contains any special characters
